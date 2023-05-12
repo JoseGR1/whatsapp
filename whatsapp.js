40a00dc1e8d71d774c07b51eda6fc46fa55b7afe
JoseGR1 <120201294+JoseGR1@users.noreply.github.com>
2023-05-11 20:39:35 -0600

    Add files via upload

---
+++ whatsapp.js
@@ -0,0 +1,20 @@
+
+  document.addEventListener("DOMContentLoaded", function() {
+    var sendButton = document.getElementById("send-button");
+    var messageInput = document.getElementById("message-input");
+    var phoneNumber = "50230847600"; // Reemplaza con el número de teléfono deseado
+  
+    sendButton.addEventListener("click", function() {
+      var message = messageInput.value.trim();
+  
+      if (message !== "") {
+        var encodedMessage = encodeURIComponent(message);
+        var whatsappURL = "https://api.whatsapp.com/send?phone=" + phoneNumber + "&text=" + encodedMessage;
+  
+        // Redirigir al usuario a la aplicación de WhatsApp con el número de teléfono y mensaje predefinidos
+        window.location.href = whatsappURL;
+      }
+    });
+  });
+  
+  
